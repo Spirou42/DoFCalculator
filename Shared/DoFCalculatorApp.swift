7efe5cfba7a30e557851f42e5303130f95be8794
Carsten Müller <carstenm@mac.com>
2022-04-03 16:33:35 +0200

    Initial Commit

---
+++ Shared/DoFCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoFCalculatorApp.swift
+//  Shared
+//
+//  Created by Carsten Müller on 03.04.22.
+//
+
+import SwiftUI
+
+@main
+struct DoFCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

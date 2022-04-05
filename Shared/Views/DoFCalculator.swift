64e1756431e5bfa6dd6d61058437196506a00e56
Carsten Müller <carstenm@mac.com>
2022-04-05 15:10:19 +0200

    Adding stuf to the Calculator

---
+++ Shared/Views/DoFCalculator.swift
@@ -0,0 +1,20 @@
+//
+//  DoFCalculator.swift
+//  DoFCalculator
+//
+//  Created by Carsten Müller on 05.04.22.
+//
+
+import SwiftUI
+
+struct DoFCalculator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DoFCalculator_Previews: PreviewProvider {
+    static var previews: some View {
+        DoFCalculator()
+    }
+}
